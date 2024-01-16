d4ad4aa005dbbccd0b4c935c0c2b0bf4365def57
sos <92243200+Cyrilsos@users.noreply.github.com>
2024-01-16 19:59:01 +0530

    Update plan.tsx

--- Components/plan.tsx
+++ Components/plan.tsx
@@ -11,8 +11,8 @@ const Plan = () => {
         <button>Weekly</button>
         <button>Monthly</button>
       </div>
-      <div className="flex justify-center">
-        <div>
+      <div className="flex justify-between">
+        <div className="flex-auto bg-white text-black border-black w-1/4 ml-20 mr-10 ">
           <h1>Heading X</h1>
           <p>Duration: x Min/day</p>
           <p>For X&X bhk</p>
@@ -20,14 +20,14 @@ const Plan = () => {
           <p>Utensils & Kitchen cleaning</p>
           <p>Washing Cupons</p>
           <p>Steam Cleaning</p>
-          <p>
+          <p className="mt-3 mb-3">
             Within allocated time, you use the maid to perform do any work
             except cooking, cleaning bathrooms and risky areas.{" "}
           </p>
           <a>Terms and conditions</a>
           <button>Book Now</button>
         </div>
-        <div>
+        <div className="flex-auto bg-white text-black border-black w-1/4 ml-20 mr-10 ">
           <h1>Heading X</h1>
           <p>Duration: x Min/day</p>
           <p>For X&X bhk</p>
@@ -35,14 +35,14 @@ const Plan = () => {
           <p>Utensils & Kitchen cleaning</p>
           <p>Washing Cupons</p>
           <p>Steam Cleaning</p>
-          <p>
+          <p className="mt-3 mb-3">
             Within allocated time, you use the maid to perform do any work
             except cooking, cleaning bathrooms and risky areas.{" "}
           </p>
           <a>Terms and conditions</a>
           <button>Book Now</button>
         </div>
-        <div>
+        <div className="flex-auto bg-white text-black border-black w-1/4 ml-20 mr-10 ">
           <h1>Heading X</h1>
           <p>Duration: x Min/day</p>
           <p>For X&X bhk</p>
@@ -50,7 +50,7 @@ const Plan = () => {
           <p>Utensils & Kitchen cleaning</p>
           <p>Washing Cupons</p>
           <p>Steam Cleaning</p>
-          <p>
+          <p className="mt-3 mb-3">
             Within allocated time, you use the maid to perform do any work
             except cooking, cleaning bathrooms and risky areas.{" "}
           </p>
