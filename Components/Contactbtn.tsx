e493f24826df62a5ad801e0e96f25d269818ad9d
sos <92243200+Cyrilsos@users.noreply.github.com>
2024-01-18 19:03:51 +0530

    updated contactbtn

--- Components/Contactbtn.tsx
+++ Components/Contactbtn.tsx
@@ -1,12 +1,16 @@
 const Contactbtn = () => {
   return (
-    <div>
-      <h1> Experiance the Best Maid Services</h1>
+    <div className="flex-auto text-center h-auto mb-40 mt-40">
+      <h1 className=" mb-5 font-extrabold text-3xl ">
+        Experiance the Best Maid Services
+      </h1>
       <p>
         Our Professional maids will take care of all your cleaning and cooking
         needs.
       </p>
-      <button>Contact Us</button>
+      <button className="bg-orange-600 hover:bg-orange-500 inline-flex items-center px-4 py-2 text-white text-sm font-medium rounded-md mt-10">
+        Contact Us
+      </button>
     </div>
   );
 };
