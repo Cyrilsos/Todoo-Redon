90043e9406f6ea8ab09d7511866fe4a631f178af
sos <92243200+Cyrilsos@users.noreply.github.com>
2024-01-24 15:27:59 +0530

    Update Footer.tsx

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -1,5 +1,19 @@
 const Footer = () => {
-  return <div></div>;
+  return (
+    <div className="flex">
+      <div>
+        <div>logo</div>
+        <div>Contact US</div>
+        <div>map</div>
+      </div>
+
+      <p>
+        <h1>Address</h1>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, se
+      </p>
+      <div>Social media</div>
+    </div>
+  );
 };
 
 export default Footer;
