f389d81ab6da0ec8d65e323da309f52c630581aa
sos <92243200+Cyrilsos@users.noreply.github.com>
2024-01-24 15:33:07 +0530

    Update Footer.tsx

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -3,7 +3,18 @@ const Footer = () => {
     <div className="flex">
       <div>
         <div>logo</div>
-        <div>Contact US</div>
+        <div className="flex">
+          Contact US
+          <input
+            type="email"
+            id="UserEmail"
+            placeholder="john@rhcp.com"
+            className="mt-1 w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
+          />
+          <button className="bg-orange-600 hover:bg-orange-500 inline-flex items-center px-4 py-2 text-white text-sm font-medium rounded-md mt-10">
+            Send
+          </button>
+        </div>
         <div>map</div>
       </div>
 
