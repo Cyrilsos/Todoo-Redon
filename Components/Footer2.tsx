b23383c74ca955ee2ae77ce039e9aece7b4adac3
sos <92243200+Cyrilsos@users.noreply.github.com>
2024-01-25 18:33:24 +0530

    Update Footer2.tsx

--- Components/Footer2.tsx
+++ Components/Footer2.tsx
@@ -1,5 +1,12 @@
 const Footer2 = () => {
-  return <div className="flex"></div>;
+  return (
+    <div className="flex">
+      <p>@ 20xx Name. All rights reserved</p>
+      <div>
+        <a>Privacy Policy</a>
+      </div>
+    </div>
+  );
 };
 
 export default Footer2;
