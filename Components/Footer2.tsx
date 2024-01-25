ddec1ab8324e028b7548c25cd233ef8f7b34f0d4
sos <92243200+Cyrilsos@users.noreply.github.com>
2024-01-25 18:39:20 +0530

    update Footer2

--- Components/Footer2.tsx
+++ Components/Footer2.tsx
@@ -1,8 +1,8 @@
 const Footer2 = () => {
   return (
-    <div className="flex">
+    <div className="flex space-x-80 place-content-center">
       <p>@ 20xx Name. All rights reserved</p>
-      <div>
+      <div className=" space-x-5">
         <a>Privacy Policy</a>
         <a>Terms of Service</a>
         <a>Cookies settings</a>
