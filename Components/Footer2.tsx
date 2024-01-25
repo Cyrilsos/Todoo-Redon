e6d7b72d2437e9941e027dc362233a5726dcb105
sos <92243200+Cyrilsos@users.noreply.github.com>
2024-01-25 18:34:15 +0530

    Update Footer2.tsx

--- Components/Footer2.tsx
+++ Components/Footer2.tsx
@@ -4,6 +4,8 @@ const Footer2 = () => {
       <p>@ 20xx Name. All rights reserved</p>
       <div>
         <a>Privacy Policy</a>
+        <a>Terms of Service</a>
+        <a>Cookies settings</a>
       </div>
     </div>
   );
